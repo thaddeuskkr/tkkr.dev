aeaec3b5ab5323933ffdd1fdc68830ba01d35aed
Thaddeus Kuah <tk@tkkr.dev>
2024-07-24 14:26:20 +0800

    feat: add play site
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

---
+++ server.js
@@ -0,0 +1,140 @@
+import { WebSocketServer } from 'ws';
+import crypto from 'node:crypto';
+
+const SERVER_HOST = '0.0.0.0';
+const SERVER_PORT = 2006;
+
+const wss = new WebSocketServer({
+    host: SERVER_HOST,
+    port: SERVER_PORT
+});
+
+const games = [];
+
+wss.on('listening', () => console.log(`WebSocket server started on ${SERVER_HOST}:${SERVER_PORT}`));
+wss.on('error', console.error);
+wss.on('connection', (ws, req) => {
+    ws.on('error', console.error);
+    console.log(`Connection established from ${req.socket.remoteAddress}`);
+    const id = crypto.randomBytes(2).toString('hex');
+    ws.playerId = id;
+    ws.inGame = false;
+    ws.on('close', () => {
+        console.log(`Connection closed from ${req.socket.remoteAddress}`);
+        const game = games.find((game) => game.player1.ws === ws || game.player2.ws === ws);
+        if (game) {
+            game.player1.ws.inGame = false;
+            game.player2.ws.inGame = false;
+            game.player1.ws.send(JSON.stringify({ type: 'endGame', reason: 'Opponent disconnected' }));
+            game.player2.ws.send(JSON.stringify({ type: 'endGame', reason: 'Opponent disconnected' }));
+            games.splice(games.indexOf(game), 1);
+        }
+    });
+    ws.send(JSON.stringify({ type: 'id', id }));
+    ws.on('message', (rawData, isBinary) => {
+        const message = isBinary ? rawData : rawData.toString();
+        const data = JSON.parse(message);
+        console.log(data);
+        switch (data.type) {
+            case 'connect': {
+                const clients = Array.from(wss.clients);
+                const validIds = clients.filter((client) => !client.inGame).map((client) => client.playerId.toLowerCase());
+                if (!validIds.includes(data.id.toLowerCase())) return ws.send(JSON.stringify({ type: 'error', message: 'Invalid player ID' }));
+                if (data.id === ws.playerId) return ws.send(JSON.stringify({ type: 'error', message: 'You cannot play with yourself' }));
+                const client = clients.find((client) => client.playerId.toLowerCase() === data.id.toLowerCase());
+                client.send(JSON.stringify({ type: 'requestConnection', id: ws.playerId }));
+                break;
+            }
+            case 'acceptConnection': {
+                const clients = Array.from(wss.clients);
+                const client = clients.find((client) => client.playerId === data.id);
+                if (!client) return ws.send(JSON.stringify({ type: 'error', message: 'Invalid player ID' }));
+                ws.inGame = true;
+                client.inGame = true;
+                const game = {
+                    player1: {
+                        player: 1,
+                        ws: ws,
+                        id: ws.playerId
+                    },
+                    player2: {
+                        player: 2,
+                        ws: client,
+                        id: client.playerId
+                    },
+                    board: [
+                        [0, 0, 0],
+                        [0, 0, 0],
+                        [0, 0, 0]
+                    ],
+                    currentTurn: 1
+                };
+                games.push(game);
+                ws.send(JSON.stringify({ type: 'startGame', player: 1 }));
+                client.send(JSON.stringify({ type: 'startGame', player: 2 }));
+                ws.send(
+                    JSON.stringify({
+                        type: 'update',
+                        game: game.board,
+                        currentTurn: game.currentTurn
+                    })
+                );
+                client.send(
+                    JSON.stringify({
+                        type: 'update',
+                        game: game.board,
+                        currentTurn: game.currentTurn
+                    })
+                );
+                break;
+            }
+            case 'move': {
+                const game = games.find((game) => game.player1.ws === ws || game.player2.ws === ws);
+                if (!game) return ws.send(JSON.stringify({ type: 'error', message: 'You are not in a game' }));
+                const player = game.player1.ws === ws ? 1 : 2;
+                if (game.currentTurn !== player) return ws.send(JSON.stringify({ type: 'alert', title: 'Invalid move: ', message: 'It is not your turn' }));
+                const cell = data.cell;
+                if (cell < 0 || cell > 8) return ws.send(JSON.stringify({ type: 'alert', title: 'Invalid move: ', message: 'Invalid cell' }));
+                const row = Math.floor(cell / 3);
+                const col = cell % 3;
+                if (game.board[row][col] !== 0) return ws.send(JSON.stringify({ type: 'alert', title: 'Invalid move: ', message: 'Cell already occupied' }));
+                game.board[row][col] = player;
+                const winner = checkWinner(game.board);
+                if (winner === 0) {
+                    game.currentTurn = game.currentTurn === 1 ? 2 : 1;
+                    game.player1.ws.send(
+                        JSON.stringify({
+                            type: 'update',
+                            game: game.board,
+                            currentTurn: game.currentTurn
+                        })
+                    );
+                    game.player2.ws.send(
+                        JSON.stringify({
+                            type: 'update',
+                            game: game.board,
+                            currentTurn: game.currentTurn
+                        })
+                    );
+                } else {
+                    game.player1.ws.inGame = false;
+                    game.player2.ws.inGame = false;
+                    game.player1.ws.send(JSON.stringify({ type: 'endGame', reason: winner === 3 ? 'Draw' : `Player ${winner} (${winner === 1 ? 'X' : 'O'}) won` }));
+                    game.player2.ws.send(JSON.stringify({ type: 'endGame', reason: winner === 3 ? 'Draw' : `Player ${winner} (${winner === 1 ? 'X' : 'O'}) won` }));
+                    games.splice(games.indexOf(game), 1);
+                }
+            }
+        }
+    });
+});
+
+function checkWinner(board) {
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] !== 0 && board[i][0] === board[i][1] && board[i][0] === board[i][2]) return board[i][0];
+        if (board[0][i] !== 0 && board[0][i] === board[1][i] && board[0][i] === board[2][i]) return board[0][i];
+    }
+    if (board[0][0] !== 0 && board[0][0] === board[1][1] && board[0][0] === board[2][2]) return board[0][0];
+    if (board[0][2] !== 0 && board[0][2] === board[1][1] && board[0][2] === board[2][0]) return board[0][2];
+    if (board.every((row) => row.every((cell) => cell !== 0))) return 3;
+    return 0;
+}
---
+++ src/js/play.js
@@ -0,0 +1,110 @@
+const WEBSOCKET_URL = 'wss://tk.is-a.dev';
+let player = 0;
+$(() => {
+    const ws = new WebSocket(WEBSOCKET_URL);
+    ws.addEventListener('error', console.error);
+    ws.addEventListener('open', () => {
+        console.log(`Connected to WebSocket server @ ${WEBSOCKET_URL}`);
+        $('.websocket-connected').removeClass('hidden');
+        $('.websocket-disconnected').addClass('hidden');
+    });
+    ws.addEventListener('close', () => {
+        console.log(`Disconnected from WebSocket server @ ${WEBSOCKET_URL}`);
+        $('.websocket-disconnected').removeClass('hidden');
+        $('.websocket-connected').addClass('hidden');
+        $('.game').addClass('hidden');
+    });
+    ws.addEventListener('message', (message) => {
+        const data = JSON.parse(message.data);
+        console.log(data);
+        switch (data.type) {
+            case 'id': {
+                console.log(`Player ID: ${data.id}`);
+                $('#playerId').html(`${data.id}`);
+                break;
+            }
+            case 'requestConnection': {
+                console.log(`Connection request from ${data.id}`);
+                $('#connectionRequests').append(`
+                    <a class="connection-request cursor-pointer text-center font-bold text-teal">Connection request from <span class="code rounded-md bg-surface0 px-1 text-teal">${data.id}</span>, click on this message to accept.</a>
+                `);
+                break;
+            }
+            case 'startGame': {
+                player = data.player;
+                $('.pregame').addClass('hidden');
+                $('.game').addClass('flex');
+                $('.game').removeClass('hidden');
+                break;
+            }
+            case 'endGame': {
+                sendAlert('Game ended: ', data.reason);
+                $('.pregame').removeClass('hidden');
+                $('.game').addClass('hidden');
+                $('.game').removeClass('flex');
+                break;
+            }
+            case 'update': {
+                for (let i = 0; i < data.game.length; i++)
+                    for (let j = 0; j < data.game[i].length; j++) {
+                        if (data.game[i][j] === 1) {
+                            $(`.game-cell[data-id="${i * 3 + j}"]`).text('X');
+                            $(`.game-cell[data-id="${i * 3 + j}"]`).removeClass('cursor-pointer');
+                        } else if (data.game[i][j] === 2) {
+                            $(`.game-cell[data-id="${i * 3 + j}"]`).text('O');
+                            $(`.game-cell[data-id="${i * 3 + j}"]`).removeClass('cursor-pointer');
+                        } else {
+                            $(`.game-cell[data-id="${i * 3 + j}"]`).text('');
+                            if (data.currentTurn === player) {
+                                $(`.game-cell[data-id="${i * 3 + j}"]`).addClass('cursor-pointer');
+                                sendAlert(`Player ${player}: `, 'It is your turn to play');
+                            } else $(`.game-cell[data-id="${i * 3 + j}"]`).removeClass('cursor-pointer');
+                        }
+                    }
+                break;
+            }
+            case 'error': {
+                sendAlert('Connection failed: ', data.message);
+                break;
+            }
+            case 'alert': {
+                sendAlert(data.title, data.message);
+                break;
+            }
+        }
+    });
+
+    $('.game-cell').on('click', function () {
+        const cell = $(this).data('id');
+        ws.send(JSON.stringify({ type: 'move', cell }));
+    });
+
+    $(document).on('click', '.connection-request', function () {
+        const connectingId = $(this).find('span').text();
+        ws.send(JSON.stringify({ type: 'acceptConnection', id: connectingId }));
+        $(this).remove();
+    });
+
+    $('#connect').on('click', () => {
+        ws.send(JSON.stringify({ type: 'connect', id: $('#multiId').val() }));
+        $('#multiId').val('');
+    });
+
+    function sendAlert(title, data) {
+        $('#alert-title').text(title);
+        $('#alert-data').text(data);
+        $('#alert').removeClass('hidden');
+        $('#alert').removeClass('opacity-0');
+        $('#alert').addClass('opacity-100');
+        // eslint-disable-next-line no-undef
+        alertTimeout = setTimeout(() => {
+            $('#alert').removeClass('opacity-100');
+            $('#alert').addClass('opacity-0');
+            $('#alert').on('transitionend', () => {
+                $('#alert').addClass('hidden');
+                // eslint-disable-next-line no-undef
+                clearTimeout(alertTimeout);
+            });
+        }, 2000);
+    }
+});
