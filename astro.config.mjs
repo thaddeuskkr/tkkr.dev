b585dc3879613ffaefc23720afb771962b2af66a
Thaddeus Kuah <tk@tkkr.dev>
2025-03-25 23:10:39 +0800

    chore: revert cloudflare workers changes
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
-import cloudflare from "@astrojs/cloudflare";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    adapter: cloudflare({
-        platformProxy: {
-            enabled: true,
-        },
-    }),
 });
