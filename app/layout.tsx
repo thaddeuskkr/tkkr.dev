42187b8bc892ff12320fc0aceac801aa70666c87
Thaddeus Kuah <tk@tkkr.dev>
2025-08-27 20:23:40 +0800

    feat: add icons
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     }}
                 />
                 <title>Thaddeus Kuah</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </head>
             <body className="m-10 flex max-w-xl flex-col">
                 <Navigation />
