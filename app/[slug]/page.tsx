3950294dd7720efd1aa477302a3c460d07169cf9
Thaddeus Kuah <tk@tkkr.dev>
2025-08-28 22:21:59 +0800

    fix: 404 on slug page
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,8 @@
-import { redirect, notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 import { getUrlBySlug } from "@/lib/db";
+import * as motion from "motion/react-client";
+
+export const dynamic = "force-dynamic";
 
 export default async function Slug({
     params,
@@ -10,6 +13,30 @@ export default async function Slug({
 }) {
     const { slug } = await params;
     const urlDoc = await getUrlBySlug(slug, Object.keys(await searchParams)[0]);
-    if (!urlDoc) notFound();
+    if (!urlDoc)
+        return (
+            <main>
+                <motion.div
+                    className="pb-2"
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.2 }}>
+                    <p className="font-medium">404 - not found</p>
+                    <p>
+                        sorry, the page you’re looking for doesn’t exist.
+                        <br />
+                        if you think this is a mistake, please{" "}
+                        <a
+                            href="mailto:hi@tkkr.dev"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="underline transition-colors hover:text-purple-800 dark:hover:text-purple-300">
+                            contact me
+                        </a>
+                        .
+                    </p>
+                </motion.div>
+            </main>
+        );
     redirect(urlDoc.url);
 }
